e6bb3f6fffc61cee3cd0c9bb1861766ac3052b7a
skynh2012 <skynh20122@gmail.com>
2016-08-02 19:22:29 +0700

    first commit

---
+++ KetQuaOnline/app/src/main/java/com/duyanh/niit/ketquaonline/ui/BaseFragment.java
@@ -0,0 +1,7 @@
+package com.duyanh.niit.ketquaonline.ui;
+
+/**
+ * Created by Administrator on 01/08/2016.
+ */
+public class BaseFragment {
+}
---
+++ KetQuaOnline/app/src/main/java/com/duyanh/niit/ketquaonline/ui/activity/KetQuaMienBacFragment.java
@@ -0,0 +1,27 @@
+package com.duyanh.niit.ketquaonline.ui.activity;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+
+import com.duyanh.niit.ketquaonline.R;
+
+
+public class KetQuaMienBacFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View viewketqua = inflater.inflate(R.layout.fragment_ketquamienbac, container, false);
+
+        return viewketqua;
+    }
+}
